72b46b90f6b97d0fb2a07ea0da26a448bf2ea598
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-08-05 17:09:37 +0530

    Create Ex04_List_ScrollView.js

---
+++ my-first-app/screens/Ex04_List_ScrollView.js
@@ -0,0 +1,49 @@
+import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+
+export default function Ex04_List_ScrollView(){
+    const [studentList,setStudentList] = useState([
+        {name : 'Adam', key : 101},
+        {name : 'Barbara', key : 102},
+        {name : 'Charlie', key :103},
+        {name : 'David', key : 104},
+        {name : 'Ela', key : 105},
+        {name : 'Farida', key : 106},
+        {name : 'Gorgie', key :107},
+        {name : 'Harry', key : 108},
+        {name : 'Ishu', key : 109},
+        {name : 'Jack', key : 110}
+    ]);
+
+    return (
+        <View style={styles.container}>
+            {studentList.map (
+                student => 
+                (
+                    <Text 
+                    key={student.key}
+                    style={styles.details}>
+                        {student.name}
+                    </Text>
+                )
+            )}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex : 1,
+        justifyContent : 'flex-start',
+        alignItems : 'center'
+    },
+    details:{
+        backgroundColor : 'lightyellow',
+        width : 200,
+        marginVertical : 10,
+        padding : 15,
+        textAlign : "center",
+        fontFamily : 'serif',
+        fontSize : 25
+    }
+})
